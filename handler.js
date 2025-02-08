5dee1f7992cee0f22d138b4bfec494241fe9e902
Shiyan <shiyan2006@gmail.com>
2025-02-08 12:06:24 +0530

    update handler

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@ exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully test 3!",
+      message: "Go Serverless v4! Your function executed successfully test 4!",
     }),
   };
 };
